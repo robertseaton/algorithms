8bbe2f58b57a3aa7169c6202776ca27c4c0db351
robert seaton <robbpseaton@gmail.com>
2014-05-12 16:39:16 -0500

    Handle error case.

--- exponents.hs
+++ exponents.hs
@@ -7,7 +7,7 @@ sqr x = x * x
 pow' :: Integer -> Integer -> Integer
 pow' x 0 = 1
 pow' x n
-  | n < 0 = 0
+  | n < 0 = error "Negative exponent"
   | otherwise = x * pow x (n - 1)
 
 -- Divide and conquer, O(lg n) solution.
@@ -15,7 +15,7 @@ pow :: Integer -> Integer -> Integer
 pow x 0 = 1
 pow x 1 = x
 pow x n
-  | n < 0 = 0
+  | n < 0 = error "Negative exponent"
   | even n = sqr $ pow x (n `div` 2)
   | odd n = x * (sqr $ pow x ((n - 1) `div` 2))
 
