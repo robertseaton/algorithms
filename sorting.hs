3ffb568a935e0c3fe7c151aa0c723afe5b82b80a
robert seaton <robbpseaton@gmail.com>
2014-05-13 18:58:32 -0500

    mergesort -> mergesortBy to be more like how the Prelude does it.

--- sorting.hs
+++ sorting.hs
@@ -1,13 +1,20 @@
+module Sorting
+       ( mergesortBy )
+       where
 
+import Data.Ord (compare)
 
 -- TODO: A split that calls a length might result in worse complexity.
-mergesort :: (a -> a -> Bool) -> [a] -> [a]
-mergesort _ [] = []
-mergesort op [a] = [a]
-mergesort op a = merge op (mergesort op (fst half)) (mergesort op (snd half))
+mergesortBy :: (a -> a -> Ordering) -> [a] -> [a]
+mergesortBy _ [] = []
+mergesortBy cmp [a] = [a]
+mergesortBy cmp a = merge cmp (mergesortBy cmp (fst half)) (mergesortBy cmp (snd half))
   where half = splitAt (length a `div` 2) a
 
-merge :: (a -> a -> Bool) -> [a] -> [a] -> [a]
-merge op (x:xs) (y:ys)
-  | x `op` y = y : merge op (x:xs) ys
-  | otherwise = x : merge op xs (y:ys)
+merge :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
+merge cmp (x:xs) (y:ys)
+  | x `cmp` y == GT = y : merge cmp (x:xs) ys
+  | otherwise = x : merge cmp xs (y:ys)
+
+mergesort :: Ord a => [a] -> [a]
+mergesort = mergesortBy compare
