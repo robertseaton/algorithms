23b9f8ea8bf408e8ab2bda7e6fa1c316f0dfbcfc
robert seaton <robbpseaton@gmail.com>
2014-05-13 19:19:53 -0500

    Write mergesort test.

--- tests.hs
+++ tests.hs
@@ -3,6 +3,7 @@ import Sorting
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
+import Data.List (sort)
 
 main :: IO ()
 main = defaultMain tests
@@ -12,8 +13,13 @@ tests =
       [ testGroup "Exponents"
           [ testProperty "pow" pow_t
           , testProperty "pow'" pow_t']
+      , testGroup "Sorting"
+         [ testProperty "mergesort" mergesort_t ]
       ]
 
 pow_t, pow_t' :: Integer -> Positive Integer -> Bool
 pow_t x (Positive n) = pow x n  == x ^ n
 pow_t' x (Positive n) = pow x n  == x ^ n
+
+mergesort_t :: [Int] -> Bool
+mergesort_t xs = sort xs == mergesort xs
