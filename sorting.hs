1083e10c2824d92b58dcfaaa732ea337030fd851
robert seaton <robbpseaton@gmail.com>
2014-05-13 19:02:18 -0500

    Unnecessary import.

--- sorting.hs
+++ sorting.hs
@@ -2,12 +2,10 @@ module Sorting
        ( mergesortBy )
        where
 
-import Data.Ord (compare)
-
 -- TODO: A split that calls a length might result in worse complexity.
 mergesortBy :: (a -> a -> Ordering) -> [a] -> [a]
 mergesortBy _ [] = []
-mergesortBy cmp [a] = [a]
+mergesortBy _ [a] = [a]
 mergesortBy cmp a = merge cmp (mergesortBy cmp (fst half)) (mergesortBy cmp (snd half))
   where half = splitAt (length a `div` 2) a
 
