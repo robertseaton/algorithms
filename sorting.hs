78b00dad38ec1033bd3756dfa3b895b9ed6099bd
robert seaton <robbpseaton@gmail.com>
2014-05-13 19:21:22 -0500

    Fix bugs.

--- sorting.hs
+++ sorting.hs
@@ -1,5 +1,5 @@
 module Sorting
-       ( mergesortBy )
+       ( mergesort )
        where
 
 -- TODO: A split that calls a length might result in worse complexity.
@@ -10,6 +10,8 @@ mergesortBy cmp a = merge cmp (mergesortBy cmp (fst half)) (mergesortBy cmp (snd
   where half = splitAt (length a `div` 2) a
 
 merge :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
+merge _ x [] = x
+merge _ [] x = x
 merge cmp (x:xs) (y:ys)
   | x `cmp` y == GT = y : merge cmp (x:xs) ys
   | otherwise = x : merge cmp xs (y:ys)
