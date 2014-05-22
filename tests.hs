c061ce180414aa47ab1bdef36db00f149332941b
robert seaton <robbpseaton@gmail.com>
2014-05-22 16:55:12 -0500

    Implement selection sort tests.

--- tests.hs
+++ tests.hs
@@ -19,7 +19,8 @@ tests =
       , testGroup "Sorting"
          [ testProperty "mergesort" mergesort_t
          , testProperty "quicksort" quicksort_t
-         , testProperty "insertion sort" insertionsort_t ]
+         , testProperty "insertion sort" insertionsort_t
+         , testProperty "selection sort" selectionsort_t ]
       ]
 
 pow_t, pow_t' :: Integer -> Positive Integer -> Bool
@@ -29,10 +30,11 @@ pow_t' x (Positive n) = pow x n  == x ^ n
 sort_t :: ([Int] -> [Int]) -> [Int] -> Bool
 sort_t sortf xs = sort xs == sortf xs
 
-mergesort_t, quicksort_t, insertionsort_t :: [Int] -> Bool
+mergesort_t, quicksort_t, insertionsort_t, selectionsort_t :: [Int] -> Bool
 mergesort_t = sort_t mergesort
 quicksort_t = sort_t quicksort
 insertionsort_t = sort_t insertionsort
+selectionsort_t = sort_t selectionsort
 
 fib_t :: (Int -> Integer) -> Positive Int -> Bool
 fib_t f (Positive n) = f n == fib''' n
